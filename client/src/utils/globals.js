cbd44f2a7eee61869fbf3d57a28fd96ff8e00708
lu <Canada_wl@aliyun.com>
2019-06-11 01:09:45 -0400

    add theam

--- client/src/utils/globals.js
+++ client/src/utils/globals.js
@@ -1,5 +1,7 @@
 import logoPath from './img/Logo.svg';
+
 const rootPath = 'localhost:3000';
+
 export const imageNotFount =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/No_image_available_600_x_450.svg/600px-No_image_available_600_x_450.svg.png';
 
